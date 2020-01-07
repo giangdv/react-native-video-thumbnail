2042006333bc169a7aa431c185e16ea8aae7ffad
DinhGiang <giangdinhnd@gmail.com>
2020-01-07 11:03:29 +0700

    Add get video duration

--- ios/RNVideoThumbnail.m
+++ ios/RNVideoThumbnail.m
@@ -11,38 +11,42 @@ RCT_EXPORT_MODULE()
 RCT_EXPORT_METHOD(get:(NSString *)filepath resolve:(RCTPromiseResolveBlock)resolve
                                reject:(RCTPromiseRejectBlock)reject)
 {
-    @try {
-        filepath = [filepath stringByReplacingOccurrencesOfString:@"file://" withString:@""];
-        NSURL *vidURL = [NSURL fileURLWithPath:filepath];
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        @try {
+            NSString *newfilepath = [filepath stringByReplacingOccurrencesOfString:@"file://" withString:@""];
+            NSURL *vidURL = [NSURL fileURLWithPath:newfilepath];
 
-        AVURLAsset *asset = [[AVURLAsset alloc] initWithURL:vidURL options:nil];
-        AVAssetImageGenerator *generator = [[AVAssetImageGenerator alloc] initWithAsset:asset];
-        generator.appliesPreferredTrackTransform = YES;
+            AVURLAsset *asset = [[AVURLAsset alloc] initWithURL:vidURL options:nil];
+            AVAssetImageGenerator *generator = [[AVAssetImageGenerator alloc] initWithAsset:asset];
+            generator.appliesPreferredTrackTransform = YES;
 
-        NSError *err = NULL;
-        CMTime time = CMTimeMake(0, 60);
+            NSError *err = NULL;
+            CMTime time = CMTimeMake(0, 60);
 
-        CGImageRef imgRef = [generator copyCGImageAtTime:time actualTime:NULL error:&err];
-        UIImage *thumbnail = [UIImage imageWithCGImage:imgRef];
+            CGImageRef imgRef = [generator copyCGImageAtTime:time actualTime:NULL error:&err];
+            UIImage *thumbnail = [UIImage imageWithCGImage:imgRef];
 
-        NSMutableDictionary *result = [NSMutableDictionary new];
-        if (thumbnail) {
-            [result setObject:@(thumbnail.size.width) forKey:@"width"];
-            [result setObject:@(thumbnail.size.height) forKey:@"height"];
+            NSMutableDictionary *result = [NSMutableDictionary new];
+            if (thumbnail) {
+                CMTime duration = asset.duration;
+                [result setObject:@(thumbnail.size.width) forKey:@"width"];
+                [result setObject:@(thumbnail.size.height) forKey:@"height"];
+                [result setObject:@(CMTimeGetSeconds(duration)) forKey:@"duration"];
 
-            NSString *header = @"data:image/png;base64,";
-            NSString *imgdata = [UIImagePNGRepresentation(thumbnail) base64EncodedStringWithOptions:NSDataBase64Encoding64CharacterLineLength];
-            NSString *data = [header stringByAppendingString:imgdata];
+                NSString *header = @"data:image/png;base64,";
+                NSString *imgdata = [UIImagePNGRepresentation(thumbnail) base64EncodedStringWithOptions:NSDataBase64Encoding64CharacterLineLength];
+                NSString *data = [header stringByAppendingString:imgdata];
 
-            [result setObject:data forKey:@"data"];
+                [result setObject:data forKey:@"data"];
+            }
+            CGImageRelease(imgRef);
+
+            resolve(result);
+
+        } @catch(NSException *e) {
+            reject(e.reason, nil, nil);
         }
-        CGImageRelease(imgRef);
-
-        resolve(result);
-
-    } @catch(NSException *e) {
-        reject(e.reason, nil, nil);
-    }
+    });
 }
 
 
